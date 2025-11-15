08bb9c06803d884124e44c917adcbf48ae63ecef
Ankita Bilgaiya <ankitabilgaiya@gmail.com>
2025-11-16 00:28:15 +0530

    Lab_05.c

--- Lab_05.c
+++ Lab_05.c
@@ -3,75 +3,60 @@
 
 struct node {
     int data;
-    struct node *left, *right;
+    struct node *left;
+    struct node *right;
 };
 
-struct node* create(int data) {
-    struct node* n = (struct node*)malloc(sizeof(struct node));
-    n->data = data;
-    n->left = n->right = NULL;
-    return n;
+struct node* create() {
+    int val;
+    printf("Enter data for Node (0 to stop): ");
+    scanf("%d", &val);
+
+    if (val == 0) {
+        return NULL;
+    }
+
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->left = NULL;
+    newnode->right = NULL;
+
+    printf("Enter data for Left Child of %d:\n", val);
+    newnode->left = create();
+
+    printf("Enter data for Right Child of %d:\n", val);
+    newnode->right = create();
+
+    return newnode;
 }
 
-struct node* insert(struct node* root, int data) {
-    if(root == NULL) return create(data);
-    if(data < root->data)
-        root->left = insert(root->left, data);
-    else
-        root->right = insert(root->right, data);
-    return root;
+void Inorder(struct node *root) {
+    if (root != NULL) {
+        Inorder(root->left);
+        printf("%d ", root->data);
+        Inorder(root->right);
+    }
 }
 
-void inorder(struct node* root) {
-    if(root) { 
-        inorder(root->left); 
-        printf("%d ", root->data); 
-        inorder(root->right); }
-}
-void preorder(struct node* root) {
-    if(root) { 
-        printf("%d ", root->data); 
-        preorder(root->left); 
-        preorder(root->right); }
-}
-void postorder(struct node* root) {
-    if(root) { 
-        postorder(root->left); 
-        postorder(root->right); 
-        printf("%d ", root->data); }
-}
-
-void search(struct node* root, int key) {
-    if(root == NULL) { 
-        printf("NULL\n"); 
-        return; }
-    if(root->data == key) 
-        printf("Found\n");
-    else if(key < root->data) 
-        search(root->left, key);
-    else 
-         search(root->right, key);
+void Preorder(struct node *root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        Preorder(root->left);
+        Preorder(root->right);
+    }
 }
 
 int main() {
-    struct node* root = NULL;
-    int n, data, key;
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &data);
-        root = insert(root, data);
-    }
-    printf("Inorder: "); 
-    inorder(root);
-    printf("\nPreorder: "); 
-    preorder(root);
-    printf("\nPostorder: "); 
-    postorder(root);
-    printf("\nEnter element to search: "); 
-    scanf("%d", &key);
-    search(root, key);
+    struct node *root = NULL;
+
+    printf("Create Binary Tree:\n");
+    root = create();
+
+    printf("\nInorder Traversal: ");
+    Inorder(root);
+
+    printf("\nPreorder Traversal: ");
+    Preorder(root);
+
     return 0;
 }
-
-
