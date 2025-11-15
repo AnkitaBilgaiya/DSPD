63f7a99c09ed8c3b6e3f6a2d3c2d20553548d58c
Ankita Bilgaiya <ankitabilgaiya@gmail.com>
2025-11-16 00:27:29 +0530

    Lab_07.c

--- Lab_07.c
+++ Lab_07.c
@@ -1,52 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define N 9
-int adj[10][10] = {
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,1,0,1,0,0,0,0,0},  
-    {0,1,0,1,0,1,0,0,0,0},
-    {0,0,1,0,0,0,0,0,0,0},  
-    {0,1,0,0,0,1,0,1,0,0},  
-    {0,0,1,0,1,0,1,0,1,0},  
-    {0,0,0,0,0,1,0,0,0,0},  
-    {0,0,0,0,1,0,0,1,0,0}, 
-    {0,0,0,0,0,1,0,1,0,1},  
-    {0,0,0,0,0,0,0,0,1,0}   
-};
+#define MAX 20
+
+int adj[MAX][MAX];
+int visited[MAX];
+int queue[MAX];
+int n;
 
 void BFS(int start) {
-    int visited[10] = {0}, queue[10], front = 0, rear = 0;
+    int i;
+    int front = -1, rear = -1;
+
+    for (i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    queue[++rear] = start;
     visited[start] = 1;
-    queue[rear++] = start;
-    printf("BFS Order: ");
-    while(front < rear) {
-        int node = queue[front++];
-        printf("%d ", node);
-        for(int i = 1; i <= N; i++) {
-            if(adj[node][i] && !visited[i]) {
+
+    while (front != rear) {
+        int v = queue[++front];
+        printf("%d ", v);
+
+        for (i = 1; i <= n; i++) {
+            if (adj[v][i] == 1 && visited[i] == 0) {
+                queue[++rear] = i;
                 visited[i] = 1;
-                queue[rear++] = i;
             }
         }
     }
     printf("\n");
 }
 
-void DFS(int node, int visited[]) {
-    printf("%d ", node);
-    visited[node] = 1;
-    for(int i = 1; i <= N; i++) {
-        if(adj[node][i] && !visited[i])
-            DFS(i, visited);
+void DFS(int v) {
+    int i;
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (i = 1; i <= n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0) {
+            DFS(i);
+        }
     }
 }
 
 int main() {
-    int visited[10] = {0};
-    printf("Start Node: 1\n");
-    BFS(1);
-    printf("DFS Order: ");
-    DFS(1, visited);
-    printf("\n");
+    int i, j, s, d, start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    printf("Enter edges (s d). Enter 0 0 to stop:\n");
+    while (1) {
+        scanf("%d %d", &s, &d);
+        if (s == 0 && d == 0)
+            break;
+
+        adj[s][d] = 1;
+        adj[d][s] = 1;
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    BFS(start);
+
+    for (i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    DFS(start);
+
     return 0;
 }
